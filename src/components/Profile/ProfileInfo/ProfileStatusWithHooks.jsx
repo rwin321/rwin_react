4cc43e6617b11f7d16a86bf317c2a8c58e91dced
ervin <rwin321@gmail.com>
2020-06-26 21:03:38 +0300

    ADDED: useEffect into ProfileStatus

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
 const ProfileStatusWithHooks = (props) => {
 
@@ -10,6 +10,9 @@ const ProfileStatusWithHooks = (props) => {
     let [editMode, setEditMode] = useState(false)
     let [status, setStatus] = useState(props.status)
 
+    useEffect(() => {
+        setStatus(props.status)
+    }, [props.status])
     let activateMode = () => {
         setEditMode(true)
     }
