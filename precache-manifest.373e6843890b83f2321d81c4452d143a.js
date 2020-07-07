6b44464210e38998a908212e4037a6bc032c495d
ervin <rwin321@gmail.com>
2020-07-07 21:23:04 +0300

    Updates

---
+++ precache-manifest.373e6843890b83f2321d81c4452d143a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47e96142727746f870b1c9200b6dd5e6",
+    "url": "/rwin_react/index.html"
+  },
+  {
+    "revision": "bbdebdf6c5fe03741b45",
+    "url": "/rwin_react/static/css/3.c2058dfa.chunk.css"
+  },
+  {
+    "revision": "e707d40622077a8f7d7f",
+    "url": "/rwin_react/static/css/4.3b2f0407.chunk.css"
+  },
+  {
+    "revision": "c4261604ad51e7af9a41",
+    "url": "/rwin_react/static/css/main.768ade99.chunk.css"
+  },
+  {
+    "revision": "c410a04bd2f886877156",
+    "url": "/rwin_react/static/js/2.ab194506.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/rwin_react/static/js/2.ab194506.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbdebdf6c5fe03741b45",
+    "url": "/rwin_react/static/js/3.c3717ba4.chunk.js"
+  },
+  {
+    "revision": "e707d40622077a8f7d7f",
+    "url": "/rwin_react/static/js/4.f0b66727.chunk.js"
+  },
+  {
+    "revision": "c4261604ad51e7af9a41",
+    "url": "/rwin_react/static/js/main.e6063f3a.chunk.js"
+  },
+  {
+    "revision": "834306996ac379775bbc",
+    "url": "/rwin_react/static/js/runtime-main.2d1dde79.js"
+  },
+  {
+    "revision": "1cb669e974818e3d3ce2129f13e4db1d",
+    "url": "/rwin_react/static/media/preloader.1cb669e9.svg"
+  }
+]);
